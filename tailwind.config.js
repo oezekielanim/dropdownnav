eb8b2dfeeb190309d23e2a2030e8b04a13c65161
doublekari <kaykayyaw@gmail.com>
2023-11-22 14:22:35 +0000

    setup font family and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors:{
+      "white": 'hsl(0, 0%, 98%)',
+      "gray": 'hsl(0, 0%, 41%)',
+      "black": 'hsl(0, 0%, 8%)'
+    },
+    fontFamily:{
+      'epilogue': ['Epilogue', 'sans-serif']
+    }
   },
   plugins: [],
 };
