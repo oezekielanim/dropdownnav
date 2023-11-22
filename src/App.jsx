45a60e1ef10f29590769bb382d1db0a743bb7e79
doublekari <kaykayyaw@gmail.com>
2023-11-22 23:35:16 +0000

    import Menu component into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Navbar from "./components/navbar"
+import Menu from "./components/Menu"
 export default function App() {
   return (
+    <>
          <Navbar/>
+         <Menu />
+    </>
+
  
   )
 }
